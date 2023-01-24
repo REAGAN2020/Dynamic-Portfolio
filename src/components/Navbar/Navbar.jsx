500b0158010e9ed1af2e2699e15f8863a7bf613e
reagan <reaganteflon@gmail.com>
2023-01-24 17:40:44 +0300

    added side nav

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,17 @@
-
 import React, { useState } from 'react';
 import { HiMenuAlt4, HiX } from 'react-icons/hi';
 import { motion } from 'framer-motion';
 
-
 import { images } from '../../constants';
 import './Navbar.scss';
+
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
 
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
-        < img src={images.logo} alt="logo" />
+        <img src={images.logo} alt="logo" />
       </div>
       <ul className="app__navbar-links">
         {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
@@ -24,27 +23,28 @@ const Navbar = () => {
       </ul>
 
       <div className="app__navbar-menu">
-        <HiMenuAlt4 onclick={() => setToggle(true)} />
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
 
-        {
-          toggle && (
-            <motion.div whileInView={{ x: [300, 0] }}
-              transition={{ duration: 0.85, ease: 'easeOut' }}
-            >
-              <HiX onclick={() => setToggle(false)} />
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: 'easeOut' }}
+          >
+            <HiX onClick={() => setToggle(false)} />
+            <ul>
               {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
-                <li c key={item}>
-                  <a href={`#${item}`} onclick={() => setToggle(false)} >{item}</a>
+                <li key={item}>
+                  <a href={`#${item}`} onClick={() => setToggle(false)}>
+                    {item}
+                  </a>
                 </li>
               ))}
-            </motion.div>
-
-          )
-        }
+            </ul>
+          </motion.div>
+        )}
       </div>
-
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
